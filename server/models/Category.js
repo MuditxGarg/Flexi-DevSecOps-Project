9a890e1215ebea73df37704cde37c5aaa6f03b43
Nanditasunil <109030620+Nanditasunil@users.noreply.github.com>
2024-10-11 12:22:00 +0530

    changes with frontend

--- server/models/Category.js
+++ server/models/Category.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
-    required: 'This field is required.'
+    required: "This field is required.",
   },
   image: {
     type: String,
-    required: 'This field is required.'
+    required: "This field is required.",
   },
 });
 
-module.exports = mongoose.model('Category', categorySchema);
+module.exports = mongoose.model("Category", categorySchema);
